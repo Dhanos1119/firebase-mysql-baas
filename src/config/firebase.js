d801b6473c48103b69b872c7859fcd5d602f5b0c
Dhanoshigan Ratnaraj <dhanoshiganratnarajah2001@gmail.com>
2026-01-08 01:09:22 -0800

    latest update

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,9 +1,17 @@
 import admin from "firebase-admin";
-import serviceAccount from "./firebase-key.json" assert { type: "json" };
+import fs from "fs";
+import path from "path";
+
+const serviceAccountPath = path.resolve(
+  "src/config/firebase-key.json"
+);
+
+const serviceAccount = JSON.parse(
+  fs.readFileSync(serviceAccountPath, "utf8")
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
 export default admin;
-g
